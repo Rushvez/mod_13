d32df5897db4851b783a1a813536f7e8ecec5239
Rushvez <71974935+Rushvez@users.noreply.github.com>
2021-02-20 19:09:01 +0300

    com

--- Practice.py
+++ Practice.py
@@ -2,9 +2,9 @@ count = int(input("Введите количество билетов:"))
 S = 0
 for price in range (1, count + 1):
     age = int(input("Введите возраст посетителей:"))
-    if 0 <= age <= 18:
+    if 0 <= age <= 17:
         price = 0
-    elif 18 <= age <= 25:
+    elif 18 <= age <= 24:
         price = 990
     elif 25 <= age <= 99:
         price = 1390
